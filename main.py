00504d193b15076ed0d77732f8cadaf6caa1d5e6
Maryleb <87745753+Maryleb@users.noreply.github.com>
2023-11-25 15:39:22 +1000

    Задание 2
    Вариант 49

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
 from jinja2 import Template
 
-col = "синий зеленый красный фиолетовый лавандовый желтый"
-list_checked = [2, 5]
+amount = int(input())
 
-f_template = open('colors_template.html', 'r', encoding='utf-8-sig')
+books = [
+    {"title" : "Мастер и Маргарита",
+     "author": "Булгаков М.А.",
+     "price": 581.50},
+    {"title" : "Белая гвардия",
+    "author": "Булгаков М.А.",
+     "price": 600.00},
+    {"title" : "Война и мир",
+    "author": "Толстой Л.Н.",
+     "price": 899.99},
+    {"title" : "Анна Каренина",
+    "author": "Толстой Л.Н.",
+     "price": 450.10},
+    {"title" : "Игрок",
+    "author": "Достоевский Ф.М.",
+    "price":  234.55}
+];
+
+f_template = open('book_template.html', 'r', encoding='utf-8-sig')
 html = f_template.read()
 f_template.close()
 
 template = Template(html)
-result_html = template.render(colors=col, list_checked=list_checked)
+result_html = template.render(books=books, n=amount)
 print(result_html)
 
 f = open('test.html', 'w', encoding='utf-8-sig')
